73383cb5e3e9e0967d5a829fa141af7eea12f890
kimwells00 <wellskimberly22@gmail.com>
2023-04-25 00:07:01 -0400

    javascript exercise lvl 2 completed

--- exercises/level2.js
+++ exercises/level2.js
@@ -1,24 +1,29 @@
-function lvl2exercise1 (num1, num2) {
+function lvl2exercise1(num1, num2) {
   // Return the sum of num1 and num2
-
+  sum = num1 + num2;
+  return sum;
 }
 
-function lvl2exercise2 (num1, num2) {
+function lvl2exercise2(num1, num2) {
   // Return the difference of num1 and num2
-
+  answer = num1 - num2;
+  return answer;
 }
 
-function lvl2exercise3 (num1, num2) {
+function lvl2exercise3(num1, num2) {
   // Return the multiplication of num1 and num 2
-
+  answer = num1 * num2;
+  return answer;
 }
 
-function lvl2exercise4 (num1, num2) {
+function lvl2exercise4(num1, num2) {
   // Return the division of num1 and num2
-
+  answer = num1 / num2;
+  return answer;
 }
 
-function lvl2exercise5 (num1) {
+function lvl2exercise5(num1) {
   // Add 2 to num1 using += and return it
-
+  answer = num1 + 2;
+  return answer;
 }
