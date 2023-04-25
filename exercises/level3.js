40eed30f4cfdc5ce4c986518d4ceee9bf0dba905
kimwells00 <wellskimberly22@gmail.com>
2023-04-25 00:07:12 -0400

    javascript exercise lvl 3 completed

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,33 @@
-function lvl3exercise1 () {
+function lvl3exercise1() {
   // Create strings "hello" and a "world", return the concatenation of the two
-
+  words = ["hello", "world"];
+  added = "hello" + "world";
+  return added;
 }
 
-function lvl3exercise2 () {
+function lvl3exercise2() {
   // Create a "hello" and a 12, return the concatenation of the two
-
+  variable = ["hello", 12];
+  added = "hello" + 12;
+  return added;
 }
 
-function lvl3exercise3 () {
+function lvl3exercise3() {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+  variable = 12;
+  variable = "12";
+  return variable;
 }
 
-function lvl3exercise4 () {
+function lvl3exercise4() {
   // Create a "hello world!" string. Return the length of the string
-
+  variable = "hello world!";
+  return "hello world!".length;
 }
 
-function lvl3exercise5 () {
+function lvl3exercise5() {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  let variable = "hello world!";
+  let result = variable.indexOf("world");
+  return variable.indexOf("world");
 }
