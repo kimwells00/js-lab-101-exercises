3f88d4186203674e19a98e5aca02a47e4eb684da
kimwells00 <wellskimberly22@gmail.com>
2023-04-25 00:06:50 -0400

    javascript exercise lvl 1 completed

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,44 @@
-function lvl1exercise1 () {
+function lvl1exercise1() {
   // Declare a variable as null and return it!
-
+  const variable = null;
+  return variable;
 }
 
-function lvl1exercise2 () {
+function lvl1exercise2() {
   // Declare a variable with a number value and return it
-
+  const number = 5;
+  return number;
 }
 
-function lvl1exercise3 () {
+function lvl1exercise3() {
   // Declare a variable with a floating point number value that is not a whole number and return it
-
+  const number = 0.2;
+  return number;
 }
 
-function lvl1exercise4 () {
+function lvl1exercise4() {
   // Declare a variable and with a string value as "Hello World!" and return it
-
+  const greeting = "Hello World!";
+  return greeting;
 }
 
-function lvl1exercise5 () {
+function lvl1exercise5() {
   // Declare a variable and with an array value containing the string "Hello World!" and the number 4 and return it
-
+  const greeting = ["Hello World!", 4];
+  return greeting;
 }
 
-function lvl1exercise6 () {
+function lvl1exercise6() {
   // Declare a variable as an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+  const greeting = {
+    key1: "Hello World!",
+    key2: 4,
+  };
+  return greeting;
 }
 
-function lvl1exercise7 () {
+function lvl1exercise7() {
   // Declare a variable as a boolean value 'false' and return it
-
+  let answer = false;
+  return answer;
 }
